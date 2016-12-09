3de44c0e218db976026463b473a1f99cfeef982a
Clinton Dreisbach <clinton@dreisbach.us>
2016-12-08 21:45:20 -0500

    Add design assets

--- greentogo/greentogo/settings.py
+++ greentogo/greentogo/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'greentogo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "..", "bower_components"),
+                    os.path.join(BASE_DIR, "static"), ]
