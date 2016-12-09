bbb5e708a626fe1ba6add67c75e883dbc2ebad27
Clinton Dreisbach <clinton@dreisbach.us>
2016-12-08 20:13:51 -0500

    Install django-compressor and django_extensions

--- greentogo/greentogo/settings.py
+++ greentogo/greentogo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'compressor',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
