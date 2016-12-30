c213bbb8dff3df750c4dd9ba8b975e273870ac79
Clinton Dreisbach <clinton@dreisbach.us>
2016-12-29 22:27:51 -0500

    Add production logging

--- greentogo/greentogo/settings.py
+++ greentogo/greentogo/settings.py
@@ -154,3 +154,44 @@ if len(EMAIL_ADMINS) == 1 and not EMAIL_ADMINS[0]:
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            # Include the default Django email handler for errors
+            # This is what you'd get without configuring logging at all.
+            'mail_admins': {
+                'class': 'django.utils.log.AdminEmailHandler',
+                'level': 'ERROR',
+                # But the emails are plain text by default - HTML is nicer
+                'include_html': True,
+            },
+            # Log to a text file that can be rotated by logrotate
+            'logfile': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'filename': '/opt/greentogo/logs/django.log'
+            },
+        },
+        'loggers': {
+            # Again, default Django configuration to email unhandled exceptions
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+            # Might as well log any errors anywhere else in Django
+            'django': {
+                'handlers': ['logfile'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+            # Your own app - this assumes all your logger names start with "myapp."
+            'greentogo': {
+                'handlers': ['logfile'],
+                'level': 'WARNING', # Or maybe INFO or DEBUG
+                'propagate': False
+            },
+        },
+    }
