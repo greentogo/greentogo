cbba1ffd08790f1d8c282e9168a3eb47ef813fe1
Clinton Dreisbach <clinton@dreisbach.us>
2017-05-28 12:36:49 -0400

    Fixed Expo errors

--- g2gmobile/main.js
+++ g2gmobile/main.js
@@ -7,10 +7,23 @@ import { AppStore } from "./stores";
 const store = new AppStore();
 
 class GreenToGo extends React.Component {
+    state = { fontsAreLoaded: false };
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.setState({fontsAreLoaded: true});
+    }
+
     render() {
+        if (!this.state.fontsAreLoaded) {
+            return <Expo.AppLoading/>;
+        }
         return (
             <App store={store} />
-        )
+        );
     }
 }
 
