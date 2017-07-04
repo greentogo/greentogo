19ca3b3d84526935fb16ffe590302b5d3f7cfec9
Clinton Dreisbach <clinton@dreisbach.us>
2017-07-04 14:21:41 -0400

    Add endpoint for user and subscription info

--- greentogo/greentogo/settings.py
+++ greentogo/greentogo/settings.py
@@ -156,7 +156,10 @@ LOGIN_REDIRECT_URL = '/account/'
 # API
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated', ),
 }
 
