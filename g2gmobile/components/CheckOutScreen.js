3b4bcc1e4821936195055667b8dd7c82cbbe61a8
Erin Brown <emberbrown@gmail.com>
2017-07-05 23:30:55 -0400

    Simplify check out and return screens
    - The BarCodeScannerReader will do the work to get camera permission and also
    know how to handle the scanner read
    - The Check in and out screens will set the action and pass the navigation function to the
    Bar code scanner reader

--- g2gmobile/components/CheckOutScreen.js
+++ g2gmobile/components/CheckOutScreen.js
@@ -1,73 +1,38 @@
 import React from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 import {inject, observer} from "mobx-react";
-import styles from "../styles";
 import { Permissions } from 'expo';
 import BarCodeScannerReader from './BarCodeScannerReader';
-
-import {
-    Container,
-    Header,
-    Body,
-    Title,
-    Content,
-    Form,
-    Item,
-    Input,
-    Button,
-    List,
-    ListItem,
-    Text,
-    Icon,
-    Left,
-    Right
-} from "native-base";
-import stylesheet from "../styles";
+import { Text } from 'react-native';
 
 @inject("appStore")
 @observer
 class CheckOutScreen extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            hasCameraPermission: false
-        }
-        this.props.appStore.action = "checkOutBox";
+  constructor(props) {
+    super(props)
+    this.state = {
+        hasCameraPermission: false
     }
+    this.props.appStore.action = "check out";
+  }
 
-    static route = {
-        navigationBar: {
-            title: 'Checkout container'
-        }
+  static route = {
+    navigationBar: {
+        title: 'Checkout container'
     }
+  }
 
-    handleBarCodeRead = (data) => {
-      let url = JSON.stringify(data.data);
-      let newUrl = url.substring(0, url.length - 2);
-      let locationCode = newUrl.substr(newUrl.lastIndexOf('/') + 1);
-      this.props.appStore.locationCode = locationCode;
-      this.props.navigator.push('submission');
-    }
+  navigateNext = () => {
+    this.props.navigator.push('submission');
+  };
 
-    async componentWillMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({hasCameraPermission: status === 'granted'});
-    }
-
-    render() {
-      const { hasCameraPermission } = this.state.hasCameraPermission;
-      if (hasCameraPermission === null) {
-        return <View />;
-      } else if (hasCameraPermission === false) {
-        return <Text>No access to camera</Text>;
-      } else {
-        return (
-          <View style={{flex: 1}}>
-            <BarCodeScannerReader handleBarCodeRead={this.handleBarCodeRead}/>
-          </View>
-        );
-      }
-    }
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <BarCodeScannerReader navigateNext={this.navigateNext}/>
+      </View>
+    );
+  }
 }
 
 export default CheckOutScreen;
