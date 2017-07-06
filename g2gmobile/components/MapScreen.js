04bee332f56293de7591171ccf52f6c14e5fffea
JamesFHeath <jfheathvt@gmail.com>
2017-07-05 20:57:06 -0400

    WIP Works with dummy data, cannot get the react to wait for fetch before rendering.

--- g2gmobile/components/MapScreen.js
+++ g2gmobile/components/MapScreen.js
@@ -11,15 +11,42 @@ class MapScreen extends React.Component {
         }
     }
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            restaurants: null,
+            error: null,
+        }
+    }
+
+    componentDidMount() {
+        return fetch('https://g2g.dreisbach.us/api/v1/restaurants/', {
+                      method: 'GET'}
+                    )
+        .then((response) => response.json())
+        .then((json) => {
+            this.setState({restaurants: json.data})
+        })
+    }
+
     render() {
         return (
             <MapView style={{flex: 1}}
-                     initialRegion={{
-                latitude: 35.9940,
-                longitude: -78.8986,
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05
-            }} />
+                initialRegion={{
+                    latitude: 35.9940,
+                    longitude: -78.8986,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                }}>
+            {this.state.restaurants.map(restaurant => (
+                <MapView.Marker
+                    key={restaurant.name}
+                    coordinate={{latitude: restaurant.latitude,longitude: restaurant.longitude}}
+                    title={restaurant.name}
+                    description={restaurant.address}
+                />
+            ))}
+            </MapView>
         )
     }
 }
