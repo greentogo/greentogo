950522235832575712b333de7de36a6ca6346446
Erin Brown <emberbrown@gmail.com>
2017-07-05 23:33:01 -0400

    Let BarCodeScannerReader handle lack of camera permission
    - Added the state "barCodeScanned" to prevent a bug with the BarCodeScanner component
    where it will keep scanning even if you have navigated away and are on a new page. This fix is not ideal: if
    you were to go back to the scanner page after you had scanned, it would not scan again. However, if you return
    to the homepage and scan again it will work. This is better than it continuously scanning and messing up the routing.

--- g2gmobile/components/BarCodeScannerReader.js
+++ g2gmobile/components/BarCodeScannerReader.js
@@ -29,6 +29,9 @@ import stylesheet from "../styles";
 class BarCodeScannerReader extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            barCodeScanned: false
+        }
     }
 
     async componentWillMount() {
@@ -36,15 +39,30 @@ class BarCodeScannerReader extends React.Component {
         this.setState({hasCameraPermission: status === 'granted'});
     }
 
+    handleBarCodeRead = (data) => {
+        if ( !this.state.barCodeScanned ) {
+            let url = JSON.stringify(data.data);
+            let newUrl = url.substring(0, url.length - 2);
+            let locationCode = newUrl.substr(newUrl.lastIndexOf('/') + 1);
+            this.props.appStore.locationCode = locationCode;
+            this.setState({barCodeScanned: true});
+            this.props.navigateNext();
+        }
+    }
+
     render() {
-        return (
-            <View style={{flex: 1}}>
-                <BarCodeScanner
-                  onBarCodeRead={this.props.handleBarCodeRead}
-                  style={StyleSheet.absoluteFill}
-                />
-            </View>
-        );
+        if ( this.state.hasCameraPermission ) {
+            return (
+                <View style={{flex: 1}}>
+                    <BarCodeScanner
+                    onBarCodeRead={this.handleBarCodeRead}
+                    style={StyleSheet.absoluteFill}
+                    />
+                </View>
+            );
+        } else {
+            return <Text> No camera view </Text>
+        }
     }
 }
 
