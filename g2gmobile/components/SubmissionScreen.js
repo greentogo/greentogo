65ba70a9c3eb28ee1da85c4b3820defbcc09060e
Erin Brown <emberbrown@gmail.com>
2017-07-05 23:34:02 -0400

    Clean up the Submission page and make it presentable
    - The calls to the API are still not working, they are being rejected due to strangeness with the authorization
    token

--- g2gmobile/components/SubmissionScreen.js
+++ g2gmobile/components/SubmissionScreen.js
@@ -10,6 +10,8 @@ import { Icon, Button } from 'native-base';
 
 import { inject, observer } from 'mobx-react';
 
+const apiEndpoint = 'https://g2g.dreisbach.us/api/v1';
+
 @inject('appStore')
 @observer
 class SubmissionScreen extends React.Component {
@@ -21,7 +23,7 @@ class SubmissionScreen extends React.Component {
     }
 
     componentDidMount() {
-        console.log(authToken);
+        // Get subsciptions for the account
         let authToken = this.props.appStore.authToken;
         let myHeaders = new Headers({
             'Accept': 'application/json',
@@ -35,42 +37,57 @@ class SubmissionScreen extends React.Component {
             credentials: 'include'
         };
 
-        let request = new Request('https://g2g.dreisbach.us/api/v1/me', myInit);
-
-        console.log(request);
+        let request = new Request(`${apiEndpoint}/me`, myInit);
 
         fetch(request)
         .then((response) => console.log(response))
         .then((response) => response.json())
         .then((json) => {
-            console.log('JSON');
             console.log(json);
         })
         .catch((error) => {
-            console.log('in the error');
             console.log(error)
         })
     }
 
     static route = {
         navigationBar: {
-            title: `Check Out/In Containers`
+            title: `Check In/Out`
         }
     }
 
-
-
-    _add = () => this.setState({count: this.state.count + 1})
-    _subtract = () => {
+    add = () => this.setState({count: this.state.count + 1})
+    subtract = () => {
         if (this.state.count > 1) {
             this.setState({count: this.state.count - 1})
         }
     }
 
-    handlePressIN
+    submit = () => {
+       fetch(`${apiEndpoint}/tag`, {
+           method: 'POST',
+           headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${this.props.appStore.authToken}`
+           },
+           body: JSON.stringify({
+               subscription: this.state.subscriptionId,
+               location: this.props.appStore.locationCode
+               // @TODO should also send number of boxes
+           })
+       })
+       .then((response) => console.log(response))
+       .then((response) => response.json())
+       .then((json) => {
+           this.props.navigator.push('home');
+       })
+       .catch((error) => {
+           console.log(error);
+       });
+    }
 
     render() {
-        const code = this.props.appStore.locationCode;
         const styles = StyleSheet.create({
             centeredRow: {
                 flexDirection: 'row',
@@ -82,36 +99,46 @@ class SubmissionScreen extends React.Component {
                 color: 'white'
             },
             headerText: {
-                fontSize: 20,
-                fontWeight: '800'
+                fontSize: 18,
+                fontWeight: '800',
+                textAlign: 'center'
             }
         });
         // api/v1/me
         return (
             <View>
-                <Text>{code}</Text>
-                <View style={{marginBottom: 10}}><Text style={{textAlign: 'center'}}>How many boxes?</Text></View>
+                <View style={{marginBottom: 10}}><Text style={styles.headerText}>How many boxes to {this.props.appStore.action}?</Text></View>
                 <View style={styles.centeredRow}>
                     <Button
-                        onPress={this._add} >
+                        success
+                        onPress={this.add} >
                         <Text style={styles.icon}>+</Text>
                     </ Button>
                     <Text style={{marginLeft: 10, marginRight: 10, fontSize: 20}}>{this.state.count}</Text>
                     <Button
-                        onPress={this._subtract} >
+                        success
+                        onPress={this.subtract} >
                         <Text style={styles.icon}>-</Text>
                     </ Button>
                 </View>
                 <View>
-                    <Text style={styles.headerText}>What Subscription?</Text>
-                    <Text>{this.state.subscriptionId}</Text>
+                    <Text style={styles.headerText}>Which subscription?</Text>
                     <Picker
+                        mode="dropdown"
                         selectedValue={this.state.subscriptionId}
                         onValueChange={(itemValue, itemIndex) => this.setState({subscriptionId: itemValue})}
                     >
-                        <Picker.Item label="Hey" value="hey" />
-                        <Picker.Item label="Arnold!" value="arnold"/>
+                        <Picker.Item label="2 Box Subscription (1/2)" value="1" />
+                        <Picker.Item label="4 Box Subscription (3/4)" value="2"/>
                     </ Picker>
+                    <View style={styles.centeredRow}>
+                        <Button
+                            success
+                            onPress={this.submit}
+                        >
+                            <Text style={{color: '#FFFFFF'}}>Submit</Text>
+                        </Button>
+                    </View>
                 </View>
             </View>
         )
