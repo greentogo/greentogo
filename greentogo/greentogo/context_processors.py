78c5d907598f2bcc579fbdfd3312729fcbf451b6
Clinton Dreisbach <clinton@dreisbach.us>
2017-08-13 09:10:17 -0400

    Add a banner to admin to show env

--- greentogo/greentogo/context_processors.py
+++ greentogo/greentogo/context_processors.py
@@ -1,6 +1,15 @@
 from django.conf import settings  # import the settings file
 
 
+def _env_color(env):
+    if env == "production":
+        return '#FF4136'
+    elif env == 'staging':
+        return '#FFDC00'
+    else:
+        return '#7FDBFF'
+
+
 def django_env(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'DJANGO_ENV': settings.DJANGO_ENV}
+    return {'DJANGO_ENV': settings.DJANGO_ENV, 'DJANGO_ENV_COLOR': _env_color(settings.DJANGO_ENV)}
