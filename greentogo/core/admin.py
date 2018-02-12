05b0e8407b6c0d30b60edfd770e8cc34cccc2e63
Stephen Lindberg <stwelin@gmail.com>
2018-02-12 10:29:24 -0500

    Add admin function to reset a subscription's checkouts

--- greentogo/core/admin.py
+++ greentogo/core/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from django.http import HttpResponse
 
-from .models import Location, UnclaimedSubscription, Subscription
+from .models import Location, UnclaimedSubscription, Subscription, LocationTag
+
+def checkin_all_boxes(modeladmin, request, queryset):
+    """
+    Box count is tied to the subscription model, but we
+    want to operate on users. This function should find
+    the related subscription and perform enough check-ins
+    to reset all their boxes.
+    """
+
+    for sub in queryset:
+        #Check if subscription can check in at least 1 box
+        if sub.can_checkin():
+            #Create checkin tags equal to number of boxes currently checked out
+            for i in range(sub.boxes_checked_out()):
+                # Use the first checkin LocationTag
+                # TODO: determine best method for choosing checkin location
+                LocationTag.objects.create(subscription=sub, location=Location.objects.checkin().first())
+
+
+checkin_all_boxes.short_description = "Return all boxes for selected users"
 
 class SubscriptionAdmin(admin.ModelAdmin):
     search_fields = ('name', 'user', 'stripe_id', )
+    actions = [checkin_all_boxes]
 
 class UnclaimedSubscriptionAdmin(admin.ModelAdmin):
     list_display = ('email', 'plan', 'claimed', )
