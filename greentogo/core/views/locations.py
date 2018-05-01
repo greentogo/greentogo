900295c3136553adf1a0c4a11b71e4d49895ff5d
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-05-01 13:41:37 -0400

    add box count up for people with multiple subs

--- greentogo/core/views/locations.py
+++ greentogo/core/views/locations.py
@@ -39,11 +39,8 @@ def location(request, location_code):
 
     if request.method == "POST":
         subscription_id = request.POST.get('subscription_id')
-        print(subscription_id)
-        # TODO NOT EVERYONE HAS A SUBSCRIPTION ID!!!!
-        # TODO People Can have Multiple Subscriptions
-        # TODO for subscription_id in user, add boxes checked in
-        boxesCheckedIn = int((LocationTag.objects.filter(subscription_id=subscription_id)).count()/2)
+        for sub in user.get_subscriptions():
+            boxesCheckedIn = boxesCheckedIn + int((LocationTag.objects.filter(subscription_id=sub.id)).count()/2)
         communityBoxesCheckedIn = int((LocationTag.objects.all()).count()/2)
         number_of_boxes = int(request.POST.get('number_of_boxes', 1))
         try:
