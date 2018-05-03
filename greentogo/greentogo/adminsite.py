33911edf5f7d8f64f5bb0ac49e3d6c0898ed4f33
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-05-02 20:32:17 -0400

    add check ins csv

--- greentogo/greentogo/adminsite.py
+++ greentogo/greentogo/adminsite.py
@@ -14,7 +14,7 @@ from core.models import (
 from core.views.admin import (
     activity_report, empty_location, empty_locations, restock_location, restock_locations,
     stock_report, export_data, unclaimed_subscription_status_csv, export_total_check_out, 
-    export_total_check_in, export_check_out_by_user, 
+    export_total_check_in, export_check_out_by_user, export_check_in_by_user, 
 )
 
 from export_action.admin import export_selected_objects
@@ -137,6 +137,13 @@ admin_site.register_view(
 admin_site.register_view(
     path='export_data/core/export_check_out_by_user.csv',
     view=export_check_out_by_user,
+    name="Download CSV",
+    only_superusers=True,
+)
+
+admin_site.register_view(
+    path='export_data/core/export_check_in_by_user.csv',
+    view=export_check_in_by_user,
     name="Download CSV",
     only_superusers=True,
 )
