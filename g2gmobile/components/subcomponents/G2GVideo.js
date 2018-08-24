e12d344c1a952894ddeca9f10f5d1968703b2a0e
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-08-24 02:52:02 -0400

    add video splash screen

--- g2gmobile/components/subcomponents/G2GVideo.js
+++ g2gmobile/components/subcomponents/G2GVideo.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {
     StyleSheet,
     View,
+    Image,
     TouchableHighlight,
+    TouchableOpacity,
     ScrollView,
     Dimensions
 } from 'react-native';
@@ -23,6 +25,7 @@ class G2GVideo extends BaseScreen {
         this.state = {
             mute: false,
             shouldPlay: false,
+            videoStarted: false,
         }
     }
 
@@ -49,35 +52,55 @@ class G2GVideo extends BaseScreen {
     render() {
         const win = Dimensions.get('window');
         return (
-            <View>
-                <VideoPlayer
-                    videoProps={{
-                        shouldPlay: false,
-                        resizeMode: Video.RESIZE_MODE_CONTAIN,
-                        isMuted: false,
-                        ref: component => {
-                            this._playbackInstance = component;
-                        },
-                    }}
-                    showControlsOnLoad={true}
-                    isPortrait={this.state.isPortrait}
-                    switchToLandscape={this.switchToLandscape.bind(this)}
-                    switchToPortrait={this.switchToPortrait.bind(this)}
-                    playFromPositionMillis={0}
-                />
-                {/* <Video
-                    resizeMode="cover"
-                    shouldPlay={this.state.shouldPlay}
-                    isMuted={this.state.mute}
-                    style={{
-                        alignSelf: 'stretch',
-                        width: win.width,
-                        height: win.width / 2
-                    }}
-                    ignoreSilentSwitch="ignore"
-                    source={require('../../assets/icons/how-2-gtg.mp4')}
-                /> */}
-            </View>
+            <TouchableOpacity onPress={() => {
+                if (!this.state.videoStarted) {
+                    this.setState({ videoStarted: true });
+                }
+            }}>
+                {this.state.videoStarted ? (
+                    <VideoPlayer
+                        videoProps={{
+                            shouldPlay: false,
+                            resizeMode: Video.RESIZE_MODE_CONTAIN,
+                            isMuted: false,
+                            style: {
+                                alignSelf: 'stretch',
+                                width: win.width,
+                                height: (win.width / 2)
+                            },
+                            ref: component => {
+                                this._playbackInstance = component;
+                            },
+                        }}
+                        showControlsOnLoad={true}
+                        isPortrait={this.state.isPortrait}
+                        switchToLandscape={this.switchToLandscape.bind(this)}
+                        switchToPortrait={this.switchToPortrait.bind(this)}
+                        playFromPositionMillis={0}
+                    />
+                    /* <Video
+                        resizeMode="cover"
+                        shouldPlay={this.state.shouldPlay}
+                        isMuted={this.state.mute}
+                        style={{
+                            alignSelf: 'stretch',
+                            width: win.width,
+                            height: win.width / 2
+                        }}
+                        ignoreSilentSwitch="ignore"
+                        source={require('../../assets/icons/how-2-gtg.mp4')}
+                    /> */
+                ) : (
+                        <Image
+                            source={require('../../assets/icons/VideoHolder.png')}
+                            style={{
+                                alignSelf: 'stretch',
+                                width: win.width,
+                                height: win.width / 2
+                            }}
+                        />
+                    )}
+            </TouchableOpacity>
         )
     }
 }
