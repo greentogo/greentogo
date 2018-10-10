36113e1a5dd4b86780a8c4ed7bfe266722be730e
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-10-09 23:03:24 -0400

    update mobile application

--- g2gmobile/apiClient.js
+++ g2gmobile/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const url = 'https://app.durhamgreentogo.com/api/v1'; // Production
+const url = 'https://app.durhamgreentogo.com/api/v1'; // Production
 // const url = 'https://g2g.dreisbach.us/api/v1'; // Staging
-const url = 'https://e57f9e3a.ngrok.io/api/v1'; // Testing
+// const url = 'https://e57f9e3a.ngrok.io/api/v1'; // Testing
 
 const instance = axios.create({
   baseURL: url,
