e40d831ed216e25a8107d57275e89293a3e990de
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-10-21 11:44:17 -0400

    add total checkouts to all subs csv

--- greentogo/core/views/admin.py
+++ greentogo/core/views/admin.py
@@ -216,10 +216,10 @@ def export_subscriptions(request, *args, **kwargs):
     writer = csv.writer(response)
     subs = Subscription.objects.all()
 
-    writer.writerow(['starts at', 'ends at', 'plan', 'user', 'email', 'stripe id', 'stripe status', 'cancelled_by_user',  'corporate_code_id', 'coupon_code_id', 'is_active', ])
+    writer.writerow(['starts at', 'ends at', 'plan', 'user', 'email', 'stripe id', 'stripe status', 'cancelled_by_user',  'corporate_code_id', 'coupon_code_id', 'is_active', 'total_checkouts', ])
 
     for sub in subs:
-        writer.writerow([sub.starts_at, sub.ends_at, sub.plan, sub.user, sub.user.email, sub.stripe_id, sub.stripe_status, sub.cancelled, sub.corporate_code_id, sub.coupon_code_id, sub.is_active])
+        writer.writerow([sub.starts_at, sub.ends_at, sub.plan, sub.user, sub.user.email, sub.stripe_id, sub.stripe_status, sub.cancelled, sub.corporate_code_id, sub.coupon_code_id, sub.is_active, sub.total_checkouts()])
     return response
 
 def export_total_check_in(request, *args, **kwargs):
